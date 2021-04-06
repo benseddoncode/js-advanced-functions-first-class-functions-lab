009f07f0fe8f4204293c12cbe23bda003a78bdc7
benseddoncode <ben.seddon@aussiebb.com.au>
2021-04-06 13:52:28 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function createFareMultiplier(multiplyValue) {
+    return function (value) {
+        return multiplyValue * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2) 
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers)
+}
